d2c28c2cf7f02fde5323ff409d06dcc56722119d
Pravin.Bangad <Pravin.Bangad@bwater.com>
2025-11-17 19:53:42 -0500

    action2

--- variables.tf
+++ variables.tf
@@ -3,17 +3,17 @@ variable "region" {
 }
 
 variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+  default = "10.1.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
   type    = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+  default = ["10.1.1.0/24", "10.1.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   type    = list(string)
-  default = ["10.0.11.0/24", "10.0.12.0/24"]
+  default = ["10.1.11.0/24", "10.1.12.0/24"]
 }
 
 variable "azs" {
